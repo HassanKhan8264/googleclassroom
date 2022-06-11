6cac3b3a94a0aa3f143c3acc58f1206eaea9c7a1
Hassan Khan <hf610061@gmail.com>
2022-06-10 18:37:49 -0700

    new

---
+++ room.js
@@ -0,0 +1,11 @@
+function show() {
+    var show = document.getElementById("hidden").style.display = "block"; 
+    // document.getElementById("hidden").style.border = "thin dotted red";
+    // var value = show;
+    // hi.style.color = "#0000ff"
+    // show.style.border = "thick solid #0000FF";
+    // var value = hidden.style.size50;
+    // var value = hidden.style.height300;
+    // document.getElementById("show");
+        // document.write(value);
+}
